72319506e1e88fa48d3d34c058fad97608e1ee20
isus203 <128573607+isus203@users.noreply.github.com>
2024-04-29 20:30:03 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -35,20 +35,20 @@ AB_OTA_PARTITIONS += \
     dtbo \
     gz \
     lk \
+    tee \
+    scp \
     logo \
+    sspm \
+    spmfw \
     md1img \
     preloader \
-    product \
-    scp \
-    spmfw \
-    sspm \
     system \
+    product \
     system_ext \
-    tee \
+    vendor \
     vbmeta \
     vbmeta_system \
     vbmeta_vendor \
-    vendor \
     vendor_boot
     
 AB_OTA_POSTINSTALL_CONFIG += \
@@ -108,11 +108,3 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libgatekeeper_aidl.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
-
-# Bypass anti-rollback ROMs protection
-# Set build date to Jan 1 2009 00:00:00
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.date.utc=1230768000
-
-#TW_OVERRIDE_SYSTEM_PROPS := \
-    "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
