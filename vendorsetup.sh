f1514372af48c8ad77c7db7f109cfa539fd7e4ed
isus203 <128573607+isus203@users.noreply.github.com>
2024-07-07 15:09:14 +0300

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -38,17 +38,26 @@ export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
 export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-export OF_QUICK_BACKUP_LIST="/boot;/data;"
+export FOX_USE_BASH_SHELL=1
+export FOX_ASH_IS_BASH=1
+export OF_ENABLE_LPTOOLS=1
+export OF_QUICK_BACKUP_LIST="/boot;/data;/nvram;/proinfo;"
+export FOX_DELETE_AROMAFM=1
 export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-export FOX_EXCLUDE_NANO_EDITOR=1
+
+# OTA
+export OF_KEEP_DM_VERITY=1
+export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 
 # R12.1 Settings
 export FOX_VERSION=$(date +%y.%m.%d)-isus203
-export FOX_BUILD_TYPE="Beta"
+export FOX_BUILD_TYPE="beta"
 export OF_MAINTAINER="isus203"
 
+
 # Necessary to decrypt most laurel_sprout ROMs
-#export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
+export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
 
 # let's see what are our build VARs
 if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
