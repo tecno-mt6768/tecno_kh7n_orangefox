bb9011228bb6e70a1daefee8628eae42caec921b
isus203 <128573607+isus203@users.noreply.github.com>
2024-07-07 15:09:48 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -213,10 +213,5 @@ RECOVERY_SDCARD_ON_DATA     := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
 
-# PBRP_config
-#PB_GO := true 
-#PB_TORCH_PATH := "/sys/devices/virtual/torch/torch/torch_level"
-#PB_TORCH_MAX_BRIGHTNESS := 1
-
 # Device 
 TW_DEVICE_VERSION := Tecno Spark 9 Pro - isus203
